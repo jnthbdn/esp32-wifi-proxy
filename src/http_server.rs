a2195ff2d83d9e787865930bc15b896d08f75eec
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-07-31 21:55:12 +0200

    Refactor + add endpoint

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,15 +1,19 @@
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
+use esp_idf_svc::http::server::{EspHttpConnection, Request};
+use esp_idf_svc::mqtt::client::{EspMqttClient, QoS};
 use esp_idf_svc::{
     http::{self, server::EspHttpServer, Method},
     io::Write,
     wifi::{BlockingWifi, EspWifi},
 };
-use serde_json::Value;
+use serde_json::{Map, Value};
 use url_encoded_data::UrlEncodedData;
 
 use crate::{nvs_configuration::NvsConfiguration, template};
+
+const JSON_MANDATORY_KEYS: &[&str] = &["id"];
 
 pub fn create_http_config_server<'a>(
     mutex_config: Arc<Mutex<NvsConfiguration>>,
@@ -127,60 +131,123 @@ pub fn create_http_config_server<'a>(
     Ok(server)
 }
 
-pub fn create_http_server<'a>() -> anyhow::Result<EspHttpServer<'a>> {
+pub fn create_http_server<'a>(
+    mutex_mqtt: Arc<Mutex<EspMqttClient<'static>>>,
+) -> anyhow::Result<EspHttpServer<'a>> {
     log::info!("Creating HTTP server.");
     let mut server = EspHttpServer::new(&http::server::Configuration {
         stack_size: 10240,
         ..Default::default()
     })?;
 
-    server.fn_handler::<anyhow::Error, _>("/send_json", Method::Post, |mut req| {
-        let len_body = req
-            .header("Content-Length")
-            .and_then(|v| v.parse::<u64>().ok())
-            .unwrap_or(0);
+    let mqtt = mutex_mqtt.clone();
+    server.fn_handler::<anyhow::Error, _>(
+        "/send_soil_moisture",
+        Method::Post,
+        move |mut req| {
+            let json = extract_json_from_request(&mut req);
 
-        let mut error_message = String::new();
+            if json.is_err() {
+                req.into_status_response(400)?
+                    .write_all(json.as_ref().err().unwrap().as_bytes())?;
+                return Ok(());
+            }
 
-        if len_body == 0 {
-            error_message = "No body or no content-length".to_string();
-        } else if len_body >= 256 {
-            error_message = "Content-length too long.".to_string();
-        } else {
-            let mut buffer = [0u8; 256];
+            let json = json.unwrap();
 
-            match req.read(&mut buffer) {
-                Result::Ok(bytes_read) => {
-                    let post_str = String::from_utf8(buffer[0..bytes_read].to_vec())?;
-                    let json_data = serde_json::from_str::<Value>(&post_str);
+            if !object_contains_keys(&json, &["soil_moisture"]) {
+                req.into_status_response(400)?
+                    .write_all("Missing keys".as_bytes())?;
+                return Ok(());
+            }
 
-                    if json_data.is_err() {
-                        error_message = "Invalid JSON".to_string();
-                        log::error!("Invalid JSON ({})", json_data.err().unwrap());
-                    } else {
-                        let json_data = json_data.unwrap();
-                        let json_data = json_data.as_object();
+            if json["soil_moisture"].is_f64() {
+                mqtt.lock().unwrap().publish(
+                    &format!("soil_moisture/{}", json["id"].as_str().unwrap()),
+                    QoS::AtLeastOnce,
+                    false,
+                    format!("{:.2}", json["soil_moisture"].as_f64().unwrap()).as_bytes(),
+                )?;
+            }
 
-                        if json_data.is_none() {
-                            error_message = "Invalid JSON format".to_string();
-                        } else {
-                            let json_data = json_data.unwrap();
-                            error_message += "Save successfully!";
-                            log::info!("Json data: {:?}", json_data);
-                        }
-                    }
-                }
-                Err(_) => {
-                    error_message = "Save error: Failed to read request.".to_string();
-                }
-            };
+            req.into_status_response(200)?;
+            Ok(())
+        },
+    )?;
+
+    let mqtt = mutex_mqtt.clone();
+    server.fn_handler::<anyhow::Error, _>("/send_water_level", Method::Post, move |mut req| {
+        let json = extract_json_from_request(&mut req);
+
+        if json.is_err() {
+            req.into_status_response(400)?
+                .write_all(json.as_ref().err().unwrap().as_bytes())?;
+            return Ok(());
         }
 
-        req.into_ok_response()?
-            .write_all(error_message.as_bytes())?;
+        let json = json.unwrap();
 
+        if !object_contains_keys(&json, &["water_level"]) {
+            req.into_status_response(400)?
+                .write_all("Missing keys".as_bytes())?;
+            return Ok(());
+        }
+
+        if json["water_level"].is_f64() {
+            mqtt.lock().unwrap().publish(
+                &format!("water_level/{}", json["id"].as_str().unwrap()),
+                QoS::AtLeastOnce,
+                false,
+                format!("{:.2}", json["water_level"].as_f64().unwrap()).as_bytes(),
+            )?;
+        }
+
+        req.into_status_response(200)?;
         Ok(())
     })?;
 
     Ok(server)
+}
+
+fn object_contains_keys(json: &Map<String, Value>, additional_key: &[&str]) -> bool {
+    JSON_MANDATORY_KEYS.iter().all(|&x| json.contains_key(x))
+        && additional_key.iter().all(|&x| json.contains_key(x))
+}
+
+fn extract_json_from_request(
+    req: &mut Request<&mut EspHttpConnection>,
+) -> Result<Map<String, Value>, &'static str> {
+    let len_body = req
+        .header("Content-Length")
+        .and_then(|v| v.parse::<u64>().ok())
+        .unwrap_or(0);
+
+    if len_body == 0 {
+        return Err("No body or no content-length");
+    } else if len_body >= 256 {
+        return Err("Content-length too long.");
+    } else {
+        let mut buffer = [0u8; 256];
+
+        match req.read(&mut buffer) {
+            Ok(bytes_read) => {
+                let post_str = String::from_utf8(buffer[0..bytes_read].to_vec()).unwrap();
+
+                match serde_json::from_str::<Value>(&post_str) {
+                    Ok(json_value) => match json_value {
+                        Value::Object(obj) => Ok(obj),
+                        _ => Err("Invalid JSON (no object)"),
+                    },
+                    Err(e) => {
+                        log::error!("Invalid JSON (Error: {}).", e);
+                        Err("Invalid JSON")
+                    }
+                }
+            }
+            Err(e) => {
+                log::error!("Read error: {}", e);
+                Err("Save error: Failed to read request.")
+            }
+        }
+    }
 }
