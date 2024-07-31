0b02e0841bf6e95035d0f2fc4a778e5a6e101a92
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-07-31 17:38:13 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,117 @@
 use std::{
-    sync::{LazyLock, Mutex},
+    sync::{Arc, Mutex},
     time::SystemTime,
 };
 
 use esp_idf_svc::{
     hal::{
         delay::FreeRtos,
-        gpio::{PinDriver, Pull},
+        gpio::{Output, OutputPin, PinDriver, Pull},
         peripherals::Peripherals,
     },
-    http::{self, server::EspHttpServer, Method},
-    io::Write,
+    http::server::EspHttpServer,
+    mqtt::client::{EspMqttClient, MqttClientConfiguration},
+    wifi::{BlockingWifi, EspWifi},
 };
+
+use http_server::{create_http_config_server, create_http_server};
 use nvs_configuration::NvsConfiguration;
 use on_board_led::OnBoardLed;
-use url_encoded_data::UrlEncodedData;
-use wifi_helper::create_ap;
+use wifi_helper::{create_ap_sta_wifi, create_ap_wifi};
 
+mod http_server;
 mod nvs_configuration;
 mod on_board_led;
 mod string_error;
 mod template;
 mod wifi_helper;
 
+const MQTT_CLIENT_ID: &str = "SENSOR_WIFI_PROXY";
+
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    let nvs_config = Arc::new(Mutex::new(NvsConfiguration::take().unwrap()));
+    let wifi: Arc<Mutex<BlockingWifi<EspWifi>>>;
+
     let peripherals = Peripherals::take()?;
     let pins = peripherals.pins;
-    static NVS_CONFIG: LazyLock<Mutex<NvsConfiguration>> =
-        LazyLock::new(|| Mutex::new(NvsConfiguration::take().unwrap()));
 
-    let mut http_server: Option<EspHttpServer> = None;
+    let _http_server: EspHttpServer;
+    let is_config_mode: bool;
     let mut leds = OnBoardLed::new(pins.gpio3, pins.gpio4, pins.gpio5)?;
 
     let mut settings_button = PinDriver::input(pins.gpio9)?;
     settings_button.set_pull(Pull::Up)?;
 
-    leds.blue.set_high()?;
-
-    log::info!("Start AP...");
-    let main_config_lock = NVS_CONFIG.lock().unwrap();
-    let ap_wifi = create_ap(
-        peripherals.modem,
-        &main_config_lock.get_ssid(),
-        &main_config_lock.get_passphrase(),
-        main_config_lock.get_hidden_ssid(),
-    );
-    drop(main_config_lock);
-
-    if ap_wifi.is_err() {
-        let ap_wifi = ap_wifi.err().unwrap();
-        let timeout = SystemTime::now();
-
-        log::error!("Failed to create AP !. Restart in 10 sec...");
-        log::error!("{}", ap_wifi);
-
-        loop {
-            leds.red.set_high()?;
-            FreeRtos::delay_ms(100);
-            leds.red.set_low()?;
-            FreeRtos::delay_ms(100);
-
-            if timeout.elapsed().unwrap().as_secs() == 10 {
-                esp_idf_svc::hal::reset::restart();
-            }
-        }
-    }
-
     log::info!("Wait 5 seconds for activation of configuration web server...");
+    leds.blue.set_high()?;
     FreeRtos::delay_ms(5000);
+    is_config_mode = settings_button.is_low();
+    leds.blue.set_low()?;
 
-    if settings_button.is_low() {
-        log::info!("Configuration web server available...");
-        http_server = Some(create_http_server(&NVS_CONFIG)?);
-    }
+    if is_config_mode {
+        log::info!("CONFIGURATION MODE");
+        let ap_wifi = create_ap_wifi(peripherals.modem, &nvs_config.lock().unwrap());
 
-    if http_server.is_none() {
-        leds.blue.set_low()?;
+        if ap_wifi.is_err() {
+            log::error!("Failed to create AP !. Restart in 5 sec...");
+            log::error!("{}", ap_wifi.as_ref().err().unwrap());
+
+            flash_led_and_restart(&mut leds.red, 5);
+        }
+
+        wifi = Arc::new(Mutex::new(ap_wifi.unwrap()));
+
+        _http_server = create_http_config_server(nvs_config.clone(), wifi.clone())?;
+    } else {
+        leds.red.set_high()?;
+
+        let ap_sta_wifi = create_ap_sta_wifi(peripherals.modem, &nvs_config.lock().unwrap());
+
+        if ap_sta_wifi.is_err() {
+            log::error!("Failed to create AP !. Restart in 5 sec...");
+            log::error!("{}", ap_sta_wifi.as_ref().err().unwrap());
+
+            flash_led_and_restart(&mut leds.red, 5);
+        }
+
+        leds.red.set_low()?;
+
+        //wifi = Arc::new(Mutex::new(ap_sta_wifi.unwrap()));
+
+        _http_server = create_http_server()?;
+
+        leds.green.set_high()?;
+
+        let mqtt_client = EspMqttClient::new_cb(
+            &make_mqtt_url(&nvs_config.lock().unwrap()),
+            &MqttClientConfiguration {
+                client_id: Some(MQTT_CLIENT_ID),
+                ..Default::default()
+            },
+            |event| {
+                log::info!("[MQTT Event]: {}", event.payload());
+            },
+        );
+
+        if mqtt_client.is_err() {
+            log::error!(
+                "Failed to connect MQTT client (Error: {:?})",
+                mqtt_client.as_ref().err().unwrap()
+            );
+
+            flash_led_and_restart(&mut leds.green, 5);
+        }
+
+        leds.green.set_low()?;
     }
 
     loop {
         FreeRtos::delay_ms(250);
 
-        if http_server.is_some() {
+        if is_config_mode {
             leds.blue.toggle()?;
         }
     }
@@ -93,73 +120,21 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn create_http_server(
-    mutex_config: &'static Mutex<NvsConfiguration>,
-) -> anyhow::Result<EspHttpServer> {
-    let mut server = EspHttpServer::new(&http::server::Configuration {
-        stack_size: 10240,
-        ..Default::default()
-    })?;
+fn flash_led_and_restart<T: OutputPin>(led: &mut PinDriver<T, Output>, timeout_sec: u64) {
+    let timeout = SystemTime::now();
 
-    server.fn_handler("/", Method::Get, |req| {
-        req.into_ok_response()?
-            .write_all(template::to_html(mutex_config, None).as_bytes())
-            .map(|_| ())
-    })?;
+    loop {
+        let _ = led.set_high();
+        FreeRtos::delay_ms(100);
+        let _ = led.set_low();
+        FreeRtos::delay_ms(100);
 
-    server.fn_handler::<anyhow::Error, _>("/", Method::Post, |mut req| {
-        let len_body = req
-            .header("Content-Length")
-            .and_then(|v| v.parse::<u64>().ok())
-            .unwrap_or(0);
-
-        let mut error_message = String::new();
-
-        if len_body == 0 {
-            error_message = "Save error: No body or no content-length".to_string();
-        } else if len_body >= 256 {
-            error_message = "Save error: Content-length too long.".to_string();
-        } else {
-            let mut buffer = [0u8; 156];
-
-            match req.read(&mut buffer) {
-                Result::Ok(bytes_read) => {
-                    let post_str = String::from_utf8(buffer[0..bytes_read].to_vec())?;
-                    let post_data = UrlEncodedData::parse_str(&post_str);
-
-                    let mut mainconfig_lock = mutex_config.lock().unwrap();
-
-                    if post_data.exists("ssid") {
-                        mainconfig_lock.set_ssid(&post_data.get_first("ssid").unwrap())?;
-                    }
-
-                    if post_data.exists("pass") {
-                        let pass = post_data.get_first("pass").unwrap();
-
-                        if pass.len() == 0 || pass.len() >= 8 {
-                            mainconfig_lock.set_passphrase(pass)?;
-                        } else {
-                            error_message =
-                                "Save warning: The passphrase minimum length is 8 characters...\n"
-                                    .to_string();
-                        }
-                    }
-
-                    mainconfig_lock.set_hidden_ssid(post_data.exists("is_hidden"))?;
-
-                    error_message += "Save successfully!";
-                }
-                Err(_) => {
-                    error_message = "Save error: Failed to read request.".to_string();
-                }
-            };
+        if timeout.elapsed().unwrap().as_secs() >= timeout_sec {
+            esp_idf_svc::hal::reset::restart();
         }
+    }
+}
 
-        req.into_ok_response()?
-            .write_all(template::to_html(mutex_config, Some(error_message)).as_bytes())?;
-
-        Ok(())
-    })?;
-
-    Ok(server)
+fn make_mqtt_url(config: &NvsConfiguration) -> String {
+    format!("{}:{}", config.get_mqtt_server(), config.get_mqtt_port())
 }
